146cbc2e62f6d0b28bd317b68d18daf35069a8ce
Fidel0405 <fidel.vasquezb@gmail.com>
2020-04-25 22:23:32 -0600

    Arreglo de los logins

--- DateWebApp01/src/java/com/datewebapp/logic/ServiceLogic.java
+++ DateWebApp01/src/java/com/datewebapp/logic/ServiceLogic.java
@@ -18,7 +18,7 @@ public class ServiceLogic extends Logic
     public List<ServiceObj> getAllServices() {
         List<ServiceObj> CList = null;
         DatabaseX CDatabase = getDatabase();
-        String strSql = "SELECT * FROM usuariosweb.servicios\n" +
+        String strSql = "SELECT * FROM usuariosweb.servicios" +
 "WHERE usuariosweb.servicios.home =1;";
         ResultSet CResult = CDatabase.executeQuery(strSql);
         
